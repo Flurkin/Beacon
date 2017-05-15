d4edd1182a09c04af4ac53ddc5d28471de3fdda5
Flurkin <mark.moore1994@gmail.com>
2017-05-15 21:24:00 +1000

    Code fixes

--- src/database.js
+++ src/database.js
@@ -9,7 +9,7 @@ require('dotenv').config({silent: true});
 // Mongoose.connect('mongodb://localhost/test');
 //Mongoose.connect('mongodb://' + Config.mongo.username + ':' + Config.mongo.password + '@' + Config.mongo.url + '/' + Config.mongo.database);
 //Mongoose.connect(Config.mongo.url);
-Mongose.connect(process.env.MONGO_HOST + '/api');
+Mongoose.connect(process.env.MONGO_HOST + '/api');
 var db = Mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error'));
